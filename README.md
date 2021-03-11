43229676af508b32f98847b366fe4027c4983ef9
Shengchen Liu <shengchen.liu1225@gmail.com>
2021-03-10 22:56:15 -0500

    image basics

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Lecture Notes of 14 Lectures on Visual SLAM:  From Theory to Practice, by Xiang Gao, Tao Zhang, Qinrui Yan and Yi Liu
+# Lecture Notes of 14 Lectures on Visual SLAM:  From Theory to Practice
+
+by Xiang Gao, Tao Zhang, Qinrui Yan and Yi Liu
 
 This is repo is forked from the author's github: [code](https://github.com/gaoxiang12/slambook2)
 
@@ -6,19 +8,23 @@ I followed the lecture and wrote all the exercise notes myself.
 
 My implementation is listed under the folder `exercise`.
 
-## [Eigen Library](exercise/ch3_eigen)
+## [2. Eigen Library](exercise/ch3_eigen)
 
-### [Use Eigen](exercise/ch3_eigen/useEigen)
+### [2.1 Use Eigen](exercise/ch3_eigen/useEigen)
 
 Demonstrates the basic operations of Eigen Matrix including initialize, edit values, transpose, multiplication, sum, trace, inverse and determinant.
 
-### [Use Geometry](exercise/ch3_eigen/useGeometry)
+### [2.2 Use Geometry](exercise/ch3_eigen/useGeometry)
 
 Demonstrates utilizing `Eigen/Geometry` for rotation and transition.  Practice the transformation between Euler, Affine and Quaternion.
 
-### [Coordinate Transform](exercise/ch3_eigen/coordinateTransform)
+### [2.3 Coordinate Transform](exercise/ch3_eigen/coordinateTransform)
 
 A small example to demonstrate the coordinate transformation.
 
+## [5. Cameras and Images](exercise/ch5_cameras_and_images)
 
+### [5.1 Image Basics](exercise/ch5_cameras_and_images/imageBasics)
+
+Demonstrates basic operations of OpenCV.  Also implement the undistortion algorithm by myself with rad-tan parameters.
 
--- exercise/ch7_pose_estimation/orb_cv.cpp
+++ exercise/ch7_pose_estimation/orb_cv.cpp
@@ -12,24 +12,24 @@ int main(int argc, char **argv) {
     cout << "usage: feature_extraction img1 img2" << endl;
     return 1;
   }
-  //-- 读取图像
+  //-- load images
   Mat img_1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
   Mat img_2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
   assert(img_1.data != nullptr && img_2.data != nullptr);
 
-  //-- 初始化
+  //-- initilialization
   std::vector<KeyPoint> keypoints_1, keypoints_2;
   Mat descriptors_1, descriptors_2;
   Ptr<FeatureDetector> detector = ORB::create();
   Ptr<DescriptorExtractor> descriptor = ORB::create();
   Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
 
-  //-- 第一步:检测 Oriented FAST 角点位置
+  //-- Step 1: calculate Oriented FAST keypoints
   chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
   detector->detect(img_1, keypoints_1);
   detector->detect(img_2, keypoints_2);
 
-  //-- 第二步:根据角点位置计算 BRIEF 描述子
+  //-- Step 2: calculate BRIEF descriptors based on the positions of Oriented FAST keypoints
   descriptor->compute(img_1, keypoints_1, descriptors_1);
   descriptor->compute(img_2, keypoints_2, descriptors_2);
   chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
   drawKeypoints(img_1, keypoints_1, outimg1, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
   imshow("ORB features", outimg1);
 
-  //-- 第三步:对两幅图像中的BRIEF描述子进行匹配，使用 Hamming 距离
+  //-- Step 3: match BRIEF descriptors of the two images using Hamming distance
   vector<DMatch> matches;
   t1 = chrono::steady_clock::now();
   matcher->match(descriptors_1, descriptors_2, matches);
@@ -48,8 +48,8 @@ int main(int argc, char **argv) {
   time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
   cout << "match ORB cost = " << time_used.count() << " seconds. " << endl;
 
-  //-- 第四步:匹配点对筛选
-  // 计算最小距离和最大距离
+  //-- Step 4: select correct matching (filtering)
+  //  calculate the max & min distances
   auto min_max = minmax_element(matches.begin(), matches.end(),
                                 [](const DMatch &m1, const DMatch &m2) { return m1.distance < m2.distance; });
   double min_dist = min_max.first->distance;
@@ -58,7 +58,8 @@ int main(int argc, char **argv) {
   printf("-- Max dist : %f \n", max_dist);
   printf("-- Min dist : %f \n", min_dist);
 
-  //当描述子之间的距离大于两倍的最小距离时,即认为匹配有误.但有时候最小距离会非常小,设置一个经验值30作为下限.
+  // When the distance between the descriptors is greater than 2 times the min distance, we treat the matching as wrong.
+  // But sometimes the min distance could be very small, set an experience value of 30 as the lower bound.
   std::vector<DMatch> good_matches;
   for (int i = 0; i < descriptors_1.rows; i++) {
     if (matches[i].distance <= max(2 * min_dist, 30.0)) {
@@ -66,7 +67,7 @@ int main(int argc, char **argv) {
     }
   }
 
-  //-- 第五步:绘制匹配结果
+  //-- Step 5: visualize the matching result
   Mat img_match;
   Mat img_goodmatch;
   drawMatches(img_1, keypoints_1, img_2, keypoints_2, matches, img_match);
